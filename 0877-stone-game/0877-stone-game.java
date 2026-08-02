518697503dbcddeb9c3c1550b6ad9f0bcdef1857
Vivek Mani Tripathi <167604912+Vivek8840@users.noreply.github.com>
2026-08-02 14:34:55 +0530

    Time: 0 ms (100%), Space: 42.9 MB (76.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,39 +1,47 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int alice=0,bob=0;
-        int lmax=0,rmax=0;
-        int i=0,j=piles.length-1;
-        boolean flag=true;
-        while(i<j){
-            lmax=piles[i];
-            rmax=piles[j];
-            if(flag){
-                if(lmax>=rmax){
-                    alice+=lmax;
-                    i++;
-                }
-                else
-                {
-                    alice+=rmax;
-                    j--;
-                }
-                flag=false;
-            }
-            else{
-                 if(lmax<rmax){
-                    bob+=lmax;
-                    i++;
-                }
-                else
-                {
-                    bob+=rmax;
-                    j--;
-                }
-                flag=true;
-            }
+        // int alice=0,bob=0;
+        // int lmax=0,rmax=0;
+        // int i=0,j=piles.length-1;
+        // boolean flag=true;
+        // while(i<j){
+        //     lmax=piles[i];
+        //     rmax=piles[j];
+        //     if(flag){
+        //         if(lmax>=rmax){
+        //             alice+=lmax;
+        //             i++;
+        //         }
+        //         else
+        //         {
+        //             alice+=rmax;
+        //             j--;
+        //         }
+        //         flag=false;
+        //     }
+        //     else{
+        //          if(lmax<rmax){
+        //             bob+=lmax;
+        //             i++;
+        //         }
+        //         else
+        //         {
+        //             bob+=rmax;
+        //             j--;
+        //         }
+        //         flag=true;
+        //     }
 
-        }
-        return (alice>bob)? true:false;
+        // }
+        // return (alice>bob)? true:false;
+        boolean flag=true;
+        // for(int i=0;i<piles.length-1;i++){
+        //     if(piles[i]==piles[i+1])
+        //     flag=false;
+        //     else
+        //     flag=true;
+        // }
+        return flag;
         
     }
 }
