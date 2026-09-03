382699cc88113ba3de02060f0ae7a00e0a92c2f2
Vivek Mani Tripathi <167604912+Vivek8840@users.noreply.github.com>
2026-09-03 23:52:12 +0530

    Time: 1 ms (99.78%), Space: 47.1 MB (76.42%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,24 +1,36 @@
 class Solution {
     public int jump(int[] nums) {
        int n=nums.length;
-       int dp[]=new int[n];
-       Arrays.fill(dp,-1);
+    //    int dp[]=new int[n];
+    //    Arrays.fill(dp,-1);
 
-       return solve(0,n,nums,dp); 
+    //    return solve(0,n,nums,dp); 
+    int far=0;
+    int jump=0;
+    int curr=0;
+    for(int i=0;i<n-1;i++){
+        far=Math.max(far,i+nums[i]);
+        if(i==curr)
+        {
+            jump++;
+            curr=far;
+        }
     }
-    private int solve(int i,int n,int nums[],int dp[]){
-       if(i>=n-1)
-       return 0;
+    
+    return jump;
+    // private int solve(int i,int n,int nums[],int dp[]){
+    //    if(i>=n-1)
+    //    return 0;
 
-       if(i+nums[i]>=n-1)
-       return 1;
-       if(dp[i]!=-1)
-       return dp[i];
+    //    if(i+nums[i]>=n-1)
+    //    return 1;
+    //    if(dp[i]!=-1)
+    //    return dp[i];
 
-       int ans=Integer.MAX_VALUE-5;
-       for(int j=i+1;j<=nums[i]+i;j++){
-        ans=Math.min(ans,1+solve(j,n,nums,dp));
-       }
-        return dp[i]=ans;
+    //    int ans=Integer.MAX_VALUE-5;
+    //    for(int j=i+1;j<=nums[i]+i;j++){
+    //     ans=Math.min(ans,1+solve(j,n,nums,dp));
+    //    }
+    //     return dp[i]=ans;
     }
 }
