f94836e9fb51df302b4768252663be255e2e19f7
Vivek Mani Tripathi <167604912+Vivek8840@users.noreply.github.com>
2026-09-04 08:32:30 +0530

    Time: 0 ms (100%), Space: 45.1 MB (64.89%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        // int mineven=Integer.MAX_VALUE;
+        // int minodd=Integer.MAX_VALUE;
+        // for(int i:nums1){
+        //     if(i%2!=0){
+        //         minodd=Math.min(minodd,i);
+        //     }
+        //     else
+        //     mineven=Math.min(mineven,i);
+        // }
+        // if(minodd==Integer.MAX_VALUE || mineven==Integer.MAX_VALUE)
+        // return true;
+        // return false
+        return true;
+    }
+}
